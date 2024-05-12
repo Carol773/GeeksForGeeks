baa71d61a46b1d85a41b2beb826eeda7db59d2e5
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-05-12 09:42:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Frequencies of Limited Range Array Elements/frequencies-of-limited-range-array-elements.py
@@ -0,0 +1,37 @@
+class Solution:
+    #Function to count the frequency of all elements from 1 to N in the array.
+    def frequencyCount(self, arr, N, P):
+        # code here
+        freq={}
+        
+        for num in arr:
+            freq[num]=freq.get(num,0)+1
+            
+        for i in range(N):
+            arr[i]=freq.get(i+1,0)
+        return arr
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+if __name__=="__main__":
+	T=int(input())
+	while(T>0):
+		N=int(input())
+		arr=[int(x) for x in input().strip().split()]
+		P=int(input())
+		ob=Solution()
+		ob.frequencyCount(arr, N, P)
+		for i in arr:
+			print(i, end=" ")
+		print()
+		T-=1
+
+
+
+# } Driver Code Ends
