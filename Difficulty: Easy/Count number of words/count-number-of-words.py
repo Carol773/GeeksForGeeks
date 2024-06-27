0de8f9c8e575a840115ef417c73e70a24a939a51
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-06-27 10:56:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count number of words/count-number-of-words.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+
+    def countWords(self, s):
+        # code here
+        s=s.replace("\\n"," ")
+        s=s.replace("\\t"," ")
+        s=s.split()
+        return len(s)
+        
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        s = input()
+
+        solObj = Solution()
+
+        print(solObj.countWords(s))
+# } Driver Code Ends
