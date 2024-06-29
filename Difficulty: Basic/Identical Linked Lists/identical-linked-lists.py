a0e5e5357d08985a2cda708c113f57ec9f92419a
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-06-29 11:07:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Identical Linked Lists/identical-linked-lists.py
@@ -0,0 +1,87 @@
+# your task is to complete this function
+# function should return true/1 if both
+# are identical else return false/0
+'''
+# Node Class    
+class node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+        
+'''
+
+#Function to check whether two linked lists are identical or not.
+def areIdentical(head1, head2):
+    # Code here
+    cur1=head1
+    cur2=head2
+    c1=0
+    c2=0
+    
+    
+    
+    while cur1!=None and cur2!=None:
+        if cur1.data!=cur2.data:
+            return False
+        cur1=cur1.next
+        cur2=cur2.next
+    
+    if cur1 is None and cur2 is None:
+        return True
+    return False
+    
+            
+
+
+#{ 
+ # Driver Code Starts
+# Node Class
+class node:
+
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+
+# Linked List Class
+class Linked_List:
+
+    def __init__(self):
+        self.head = None
+
+    def insert(self, val):
+        new_node = node(val)
+        new_node.data = val
+        new_node.next = self.head
+        self.head = new_node
+
+
+def printList(head):
+    while head:
+        print(head.data, end=' ')
+        head = head.next
+    print()
+
+
+def createList(arr, n):
+    lis = Linked_List()
+    for i in range(n):
+        lis.insert(arr[i])
+    return lis.head
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        head1 = createList(arr, n)
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        head2 = createList(arr, n)
+        if (areIdentical(head1, head2)):
+            print('true')
+        else:
+            print('false')
+
+# } Driver Code Ends
