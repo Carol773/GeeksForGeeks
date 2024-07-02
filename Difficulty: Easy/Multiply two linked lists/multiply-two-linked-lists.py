6b86d034aee9441ff6e0ed531551092c95053a06
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-07-02 09:32:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Multiply two linked lists/multiply-two-linked-lists.py
@@ -0,0 +1,90 @@
+MOD = 10**9+7
+# your task is to complete this function
+# Function should return an integer value
+# head1 denotes head node of 1st list
+# head2 denotes head node of 2nd list
+
+'''
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+'''
+
+def multiplyTwoList(head1, head2):
+    # Code here
+    cur1=head1
+    cur2=head2
+    num1=''
+    num2=''
+    
+    while cur1!=None:
+        num1+=str(cur1.data)
+        cur1=cur1.next
+        
+    while cur2!=None:
+        num2+=str(cur2.data)
+        cur2=cur2.next
+        
+    num1=int(num1)
+    num2=int(num2)
+    res=(num1* num2)%(1000000007)
+    return res
+    
+    
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+class Linked_List:
+    def __init__(self):
+        self.head = None
+
+    def get_head(self):
+        return self.head
+
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.head.data = data
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            temp = self.head
+            while(temp.next):
+                temp=temp.next
+            temp.next = new_node
+
+def printlist(ptr):
+    while ptr!=None:
+        print(ptr.data, end=" ")
+        ptr= ptr.next
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list1.insert(i)
+        # printlist(list1.get_head())
+        # print ''
+        list2 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list2.insert(i)
+        # printlist(list2.get_head())
+        # print ''
+        print(multiplyTwoList(list1.get_head(), list2.get_head()))
+
+# } Driver Code Ends
