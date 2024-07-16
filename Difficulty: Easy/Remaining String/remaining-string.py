6c7c4f2cd6776246b0c5a452e920a574028b0a43
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-07-16 17:31:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remaining String/remaining-string.py
+++ Difficulty: Easy/Remaining String/remaining-string.py
@@ -1,21 +1,16 @@
 #User function Template for python3
 class Solution:
-	def printString(self, S, ch, count):
+	def printString(self, s, ch, count):
 		# code here
-		c=0
-		if count==0:
-		    return S
-		for i in range(len(S)):
-		    if S[i]==ch:
-		        c+=1
-		        if c==count:
-		            if S[i+1:]!='':
-		                return S[i+1:]
-		            else:
-		                return 'Empty string'
-		return 'Empty string'
+		res=''
 		
-
+		for i in range(len(s)):
+		    if s[i]==ch:
+		        count-=1
+		        if count==0:
+		            res=s[i+1:]
+		return res
+		    
 
 
 #{ 
@@ -23,15 +18,14 @@ class Solution:
 #Initial Template for Python 3
 
 if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		s = input()
-		ch = input()[0]
-		count = int(input())
-		ob = Solution()	
-		answer = ob.printString(s,ch,count)
-		
-		print(answer)
+    T = int(input())
+    for i in range(T):
+        s = input()
+        ch = input()[0]
+        count = int(input())
+        ob = Solution()
+        answer = ob.printString(s, ch, count)
 
+        print(answer)
 
 # } Driver Code Ends
