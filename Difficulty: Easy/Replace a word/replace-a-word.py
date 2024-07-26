ec01ddc189144087833189e45603fa766c8af18b
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-07-26 08:20:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Replace a word/replace-a-word.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+class Solution:
+    def replaceAll (ob, S, oldW, newW):
+        # code here 
+        
+        #S.split(oldW)
+        
+        news=S.replace(oldW,newW)
+        return news
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        S = input()
+        oldW = input()
+        newW = input()
+        
+        ob = Solution()
+        print(ob.replaceAll(S, oldW, newW))
+# } Driver Code Ends
