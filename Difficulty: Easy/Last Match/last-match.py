9a899f0af6f4b78eee1c2ceb7692ed43ca140c95
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-07-26 08:10:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Last Match/last-match.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+class Solution:
+    def findLastOccurence(self, A, B):
+        # code here 
+        
+        for i in range(len(A)-len(B),-1,-1):
+            if A[i:(i+len(B))]==B:
+                return (i+1)
+        return -1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        
+        A = input()
+        B= input()
+
+        ob = Solution()
+        print(ob.findLastOccurence(A,B))
+# } Driver Code Ends
