741369af8fead4e7e4f7c108b7698d337130be0d
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-07-28 09:06:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Divisible by 5 or not/divisible-by-5-or-not.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+
+    def isDivisibleBy5(self, Bin):
+        # code here
+        dec=int(Bin,2)
+        
+        if dec%5==0:
+            return "Yes"
+        else:
+            return "No"
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        Bin = input()
+
+        solObj = Solution()
+
+        print(solObj.isDivisibleBy5(Bin))
+# } Driver Code Ends
