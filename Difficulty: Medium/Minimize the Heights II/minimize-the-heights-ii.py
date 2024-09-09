11ac8e121b1075ce55978cb62a0e45887aa59897
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-09-09 18:05:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    def getMinDiff(self, arr,k):
+        # code here
+        arr.sort()
+        n=len(arr)
+        
+        res=arr[-1]-arr[0]
+        
+        for i in range(1,n):
+            if arr[i]>=k:
+                max_val=max(arr[i-1]+k,arr[-1]-k)
+                min_val=min(arr[0]+k,arr[i]-k)
+                res=min(res,max_val-min_val)
+        return res
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        k = int(input())
+        # n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.getMinDiff(arr, k)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
