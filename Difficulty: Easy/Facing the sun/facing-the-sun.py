753b9a24e87aa7247f00456b7aea9614097476f5
Carol773 <168558483+Carol773@users.noreply.github.com>
2024-09-20 09:15:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Facing the sun/facing-the-sun.py
+++ Difficulty: Easy/Facing the sun/facing-the-sun.py
@@ -1,34 +1,30 @@
 #User function Template for python3
+
 class Solution:
-    
-    def countBuildings(self,h, n):
+    # Returns count buildings that can see sunlight
+    def countBuildings(self, height):
         # code here
-        cur_max=0
-        count=0
+        c=0
+        max_h=0
         
-        for num in h:
-            if num>cur_max:
-                count+=1
-                cur_max=num
-        return count
+        for i in range(len(height)):
+            if height[i]>max_h:
+                max_h=height[i]
+                c+=1
+        return c
+            
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
-
-
-
-
-if __name__ == '__main__':
-    tc = int(input())
-    while tc > 0:
-        n = int(input())
-        h = list(map(int, input().strip().split()))
+if __name__ == "__main__":
+    t = int(input().strip())
+    for _ in range(t):
+        height = list(map(int, input().strip().split()))
         ob = Solution()
-        ans = ob.countBuildings(h, n)
+        ans = ob.countBuildings(height)
         print(ans)
-        tc -= 1
 
 # } Driver Code Ends
